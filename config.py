06ddfe17b391632948b38299160605abb18fff5d
praptigawade12-sys <praptigawade12@gmail.com>
2026-04-30 16:31:34 +0400

    v2

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
+import streamline as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY  = os.getenv("GROQ_API_KEY","")
+GROQ_API_KEY  = NONE
+if "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" in st.secrets:
+  GROQ_API_KEY = st.secrets["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"]
+else:
+  GROQ_API_KEY = os.getenv("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")
