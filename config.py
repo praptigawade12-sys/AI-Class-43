d3e4664c7ed10410fcbf1a9d7d9dac6f8ce6c9b6
praptigawade12-sys <praptigawade12@gmail.com>
2026-04-30 16:38:33 +0400

    Update config.py
    Added debug print statement for GROQ_API_KEY.

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY  = None
 if "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" in st.secrets:
   GROQ_API_KEY = st.secrets["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"]
 else:
   GROQ_API_KEY = os.getenv("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")
+
+print("CONFIG DEBUG:", GROQ_API_KEY)
