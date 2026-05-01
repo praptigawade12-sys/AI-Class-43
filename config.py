589b0356eb6061335647da7148b7dd1685939d82
praptigawade12-sys <praptigawade12@gmail.com>
2026-05-01 16:15:44 +0400

    v2
    Refactor GROQ_API_KEY handling and add error checking.

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ import os
 import streamlit as st
 GROQ_API_KEY = os.getenv("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")
 
-if "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" in st.secrets:
-  GROQ_API_KEY = st.secrets["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"]
-else:
-  GROQ_API_KEY = os.getenv("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")
+#if "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" in st.secrets:
+#  GROQ_API_KEY = st.secrets["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"]
+#else:
+#  GROQ_API_KEY = os.getenv("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")
+#print("CONFIG DEBUG:", GROQ_API_KEY)
 
-print("CONFIG DEBUG:", GROQ_API_KEY)
+if not GROQ_API_KEY:
+  raise ValueError("GROQ_API_KEY is missing")
