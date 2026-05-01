12b05c545e34882cc13e20de2bee41ac654ec00b
praptigawade12-sys <praptigawade12@gmail.com>
2026-05-01 16:13:27 +0400

    v2

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
+GROQ_API_KEY = os.getenv("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")
 
 if "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" in st.secrets:
   GROQ_API_KEY = st.secrets["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"]
